ecd80d9887b011a9680853cb6525ab0fb13465e7
kayode <samsonkayode@gmail.com>
2020-05-19 15:15:58 +0100

    keepASpot initial setup

---
+++ KeepASpot/src/main/java/com/kas/keepaspot/KeepASpotApplication.java
@@ -0,0 +1,13 @@
+package com.kas.keepaspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepASpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepASpotApplication.class, args);
+	}
+
+}
